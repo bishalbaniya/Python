f127e6a68223c090c8aad33405bdae5ec942b0ba
Bishal <bishal075baniya@gmail.com>
2025-05-25 16:26:32 +0545

    simple calculaor

---
+++ Simple_Calculator.py
@@ -0,0 +1,15 @@
+num1 = 2
+num2 = 10
+
+ans1 = num1 + num2
+print("Addition of ", num1, " and ", num2 , "is ", ans1)
+ans2 = num1 - num2
+print("Subtraction of ", num1, " and ", num2 , "is ", ans2)
+ans3 = num1 * num2
+print("Multiplication of ", num1, " and ", num2 , "is ", ans3)
+ans4 = num1 / num2
+print("Division of ", num1, " and ", num2 , "is ", ans4)
+ans5 = num1 % num2
+print("Modulas of ", num1, " and ", num2 , "is ", ans5)
+ans6 = num1 // num2
+print("Floor Division of ", num1, " and ", num2 , "is ", ans6)
